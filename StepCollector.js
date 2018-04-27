b390bc0d7d36e89f199c8515f2bdf0c5110d1fff
LuddeW <ludwig.wiktorsson@gmail.com>
2018-04-27 15:15:38 +0200

    Test(), returnArray to get the data

--- StepCollector.js
+++ StepCollector.js
@@ -1,35 +1,49 @@
-Object.keys(Keys).forEach( key => {
+var returnArray = [];
+
+var test = function() {
+  Object.keys(Keys).forEach(key => {
+    test.key = {}
     $.post({
-        contentType: "application/x-www-form-urlencoded",
-        url: 'https://www.googleapis.com/oauth2/v4/token',
-        data: {
-            client_id: Keys[key].client_id,
-            client_secret: Keys[key].client_secret,
-            refresh_token: Keys[key].refresh_token,
-            grant_type: 'refresh_token'
-        },
-        success: function (result) {
-            console.log(key, result.access_token)
-            $.post({
-                contentType: "application/json",
-                //contentType: "application/x-www-form-urlencoded",
-                url: "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
-                beforeSend:   function (request) {
-                    request.setRequestHeader("Authorization", "Bearer " + result.access_token);
-                    },
-                data: JSON.stringify({
-                    aggregateBy: [{ 
-                        dataTypeName: "com.google.step_count.delta", 
-                        dataSourceId: "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps" 
-                    }],
-                    bucketByTime: { durationMillis: 86400000 },
-                    startTimeMillis: 1524434400000, 
-                    endTimeMillis: Date.now()
-                }),
-                success: function(result) {
-                    console.log(JSON.stringify(result))
-                }
-            })
-        }
-    })
-})
+      contentType: "application/x-www-form-urlencoded",
+      url: "https://www.googleapis.com/oauth2/v4/token",
+      data: {
+        client_id: Keys[key].client_id,
+        client_secret: Keys[key].client_secret,
+        refresh_token: Keys[key].refresh_token,
+        grant_type: "refresh_token"
+      },
+      success: function(result) {
+        $.post({
+          contentType: "application/json",
+          //contentType: "application/x-www-form-urlencoded",
+          url:
+            "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
+          beforeSend: function(request) {
+            request.setRequestHeader(
+              "Authorization",
+              "Bearer " + result.access_token
+            );
+          },
+          data: JSON.stringify({
+            aggregateBy: [
+              {
+                dataTypeName: "com.google.step_count.delta",
+                dataSourceId:
+                  "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+              }
+            ],
+            bucketByTime: { durationMillis: 86400000 },
+            startTimeMillis: 1524434400000,
+            endTimeMillis: Date.now()
+          }),
+          success: function(result) {
+            result.bucket.forEach(function(element) {
+              returnArray.push({name: key, steps: element.dataset[0].point[0].value[0].intVal});
+            });
+            return returnArray
+          }
+        });
+      }
+    });
+  });
+}
