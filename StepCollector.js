892357696341bb35a00a7248216faa599952500c
LuddeW <ludwig.wiktorsson@gmail.com>
2018-04-27 13:55:39 +0200

    Send request for all people

--- StepCollector.js
+++ StepCollector.js
@@ -1,26 +1,35 @@
-
-$.post({
-    contentType: "application/x-www-form-urlencoded",
-    url: 'https://www.googleapis.com/oauth2/v4/token',
-    data: {
-        client_id: Keys.Martin.client_id,
-        client_secret: Keys.Martin.client_secret,
-        refresh_token: Keys.Martin.refresh_token,
-        grant_type: 'refresh_token'
-    },
-    success: function (result) {
-        $.post({
-            contentType: "application/json",
-            url: "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
-            authorization: "Bearer " + JSON.stringify(result.access_token),
-            data: {
-                "aggregateBy": [{ "dataTypeName": "com.google.step_count.delta", "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps" }],
-                "bucketByTime": { "durationMillis": 86400000 },
-                "startTimeMillis": 1524434400000, "endTimeMillis": Date.now()
-            },
-            success: function(result) {
-                console.log(JSON.stringify(result))
-            }
-        })
-    }
+Object.keys(Keys).forEach( key => {
+    $.post({
+        contentType: "application/x-www-form-urlencoded",
+        url: 'https://www.googleapis.com/oauth2/v4/token',
+        data: {
+            client_id: Keys[key].client_id,
+            client_secret: Keys[key].client_secret,
+            refresh_token: Keys[key].refresh_token,
+            grant_type: 'refresh_token'
+        },
+        success: function (result) {
+            console.log(key, result.access_token)
+            $.post({
+                contentType: "application/json",
+                //contentType: "application/x-www-form-urlencoded",
+                url: "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
+                beforeSend:   function (request) {
+                    request.setRequestHeader("Authorization", "Bearer " + result.access_token);
+                    },
+                data: JSON.stringify({
+                    aggregateBy: [{ 
+                        dataTypeName: "com.google.step_count.delta", 
+                        dataSourceId: "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps" 
+                    }],
+                    bucketByTime: { durationMillis: 86400000 },
+                    startTimeMillis: 1524434400000, 
+                    endTimeMillis: Date.now()
+                }),
+                success: function(result) {
+                    console.log(JSON.stringify(result))
+                }
+            })
+        }
+    })
 })
