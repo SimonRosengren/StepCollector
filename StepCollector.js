055eb6b600438102231984c02450ca404caafa15
simonrosengren <simonrosengren123@gmail.com>
2018-04-27 13:18:40 +0200

    sending refresh req

--- StepCollector.js
+++ StepCollector.js
@@ -1,13 +1,26 @@
-$.ajax({
-    contentType: "application/json",
+
+$.post({
+    contentType: "application/x-www-form-urlencoded",
     url: 'https://www.googleapis.com/oauth2/v4/token',
     data: {
-        client_id: '123038229909-uj34ag7klfjgioc0mksh5qeq3mi5sdq9.apps.googleusercontent.com',
-        client_secret: 'o7pDIifIQqzx4Mq3o-_LVp3R',
-        refresh_token: '1/-5Un3s4ranjnpF7bumycvcx91KCiREK6JALiZyB8e9Y6-NfPtkbYpfyMlkNkeNYf',
+        client_id: Keys.Martin.client_id,
+        client_secret: Keys.Martin.client_secret,
+        refresh_token: Keys.Martin.refresh_token,
         grant_type: 'refresh_token'
     },
-    success: function(status, result) {
-        $( "#text-output" ).html( "<strong>" + result + "</strong> degrees" );
+    success: function (result) {
+        $.post({
+            contentType: "application/json",
+            url: "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
+            authorization: "Bearer " + JSON.stringify(result.access_token),
+            data: {
+                "aggregateBy": [{ "dataTypeName": "com.google.step_count.delta", "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps" }],
+                "bucketByTime": { "durationMillis": 86400000 },
+                "startTimeMillis": 1524434400000, "endTimeMillis": Date.now()
+            },
+            success: function(result) {
+                console.log(JSON.stringify(result))
+            }
+        })
     }
 })
